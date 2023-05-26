7e2a21f5cd5d21d86c13133562c8e093f9028c18
ryo.fujimoto <ryo.fujimoto@caters.co.jp>
2023-05-26 10:58:31 +0900

    ✨envでtest制御

--- tests/unit/game/test_quiz.py
+++ tests/unit/game/test_quiz.py
@@ -6,8 +6,10 @@ from lib.get_env import get as env
 class TestCreateQuiz:
     # 接続後正しい計算ができているか
     def test__接続後正しい計算ができているか(self):
+        if env("TEST_QUIZ") == "0":
+            return
+
         apikey = env("OPENAI_API_KEY")
-        
         Chat = Quiz(apikey)
         response = Chat.start()
         assert response == "setup"
--- tests/unit/test_chatgpt.py
+++ tests/unit/test_chatgpt.py
@@ -6,11 +6,10 @@ from lib.get_env import get as env
 class TestCreateQuiz:
     # 接続後正しい計算ができているか
     def test__接続後正しい計算ができているか(self):
+        if env("TEST_CHAT") == "0":
+            return
+        
         apikey = env("OPENAI_API_KEY")
-
-        assert "2" == "2"
-        return
-
         Chat = AIChatbot(api_key=apikey)
         response = Chat.talk("1+1=")
         assert response == "2"
